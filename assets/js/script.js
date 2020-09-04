683c36a5b9b3b53213a6737b39fc9e81a52b5e3b
ginomaglaque <ginomaglaque@gmail.com>
2020-09-03 19:02:14 -0700

    UX enhancements

--- assets/js/script.js
+++ assets/js/script.js
@@ -167,7 +167,7 @@ $("#task-form-modal").on("shown.bs.modal", function() {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -204,15 +204,31 @@ $(".card .list-group").sortable({
   helper: "clone",
   activate: function(event) {
     console.log("activate", this);
+    $(this)
+      .addClass("dropover");
+    $(".bottom-trash")
+      .addClass("bottom-trash-drag");
   },
   deactivate: function(event) {
     console.log("deactivate", this);
+    $(this)
+      .addClass("dropover");
+    $(".bottom-tash")
+      .addClass("bottom-trash-drag");
   },
   over: function(event) {
     console.log("over", event.target);
+    $(event.target)
+      .addClass("dropover-active");
+    $(".bottom-trash")
+      .addClass("bottom-trash-active");
   },
   out: function(event) {
     console.log("out", event.target);
+    $(event.target)
+      .addClass("dropover-active");
+    $(".bottom-trash")
+      .addClass("bottom-trash-active");
   },
   update: function(event) {
     // array to store the task data in
@@ -288,5 +304,11 @@ var auditTask = function(taskEl) {
 
 // load tasks for the first time
 loadTasks();
+
+setInterval(function () {
+  $(".card .list-group-item").each(function (el) {
+    auditTask(el);
+  });
+}, (1000 * 60) * 30);
 
 
